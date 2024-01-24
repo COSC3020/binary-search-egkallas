9eb27e848ad4a06c4d48549b4d0a2b13736991ca
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-01-24 11:38:46 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
 function binarySearch(list, element) {
+
+    var left = 0;
+    var right = list.length - 1;
+    
+if (list[0] == element) 
+    return 0;
+
+ else
+     while (left <= right){
+
+         var mid = Math.floor((right + left) / 2);
+
+            if (list[mid] == element)
+                return mid;
+
+            else if (list[mid] < element)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
     return -1;
 }
